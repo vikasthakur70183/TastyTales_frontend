92217372a58b5bea8c97efe0829a8f03ea9d4357
Vikas Thakur <83107505+vikasthakur70183@users.noreply.github.com>
2024-10-23 10:27:46 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server : {
     proxy :{
-      "/api":"https://aa214c91-00d3-4499-b8f1-96124bef7bcb.e1-us-east-azure.choreoapps.dev"
+      "/api":"http://localhost:3000"
     },
   }
 })
